f49714b8e9d10596b19e13f2c48075329c6d8fcb
thanoskoutr <koutroubas.thanos@gmail.com>
2021-09-25 21:54:04 +0300

    Add solution for part 2

--- students/thanoskoutr/main.go
+++ students/thanoskoutr/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type Quiz struct {
@@ -17,6 +18,7 @@ type Quiz struct {
 func main() {
 	// Parse command-line flag
 	csvFilename := flag.String("csv", "problems.csv", "filename of the quiz csv")
+	timeLimit := flag.Int64("limit", 30, "time limit for each question of the quiz")
 	flag.Parse()
 
 	// Open CSV
@@ -52,22 +54,35 @@ func main() {
 		problemCount++
 	}
 
-	// Print questions, get answers and keep score
+	// Create a variable for user's answer, a total score and a channel for receiving user's answer
+	c := make(chan int)
 	score := 0
 	var userAnswer string
 
+	// Print questions, get answers and keep score
 	for i, quiz := range quizSlice {
 		// Print question
 		fmt.Printf("Problem #%v: %s = ", i+1, quiz.question)
 
-		score = getAnswer(userAnswer, quiz, score)
+		// Run getAnswer as a goroutine
+		go getAnswer(userAnswer, quiz, score, c)
+
+		// Wait for an answer, if timeLimit is exceeded stop the quiz
+		select {
+		case score = <-c:
+			continue
+		case <-time.After(time.Duration(*timeLimit) * time.Second):
+			fmt.Println()
+			fmt.Printf("You scored %v out of %v\n", score, problemCount)
+			log.Fatal("Timed Out")
+		}
 	}
 
 	// Print score
 	fmt.Printf("You scored %v out of %v\n", score, problemCount)
 }
 
-func getAnswer(userAnswer string, quiz Quiz, score int) int {
+func getAnswer(userAnswer string, quiz Quiz, score int, c chan int) {
 	// Read answer
 	fmt.Scanln(&userAnswer)
 	userAnswer = strings.TrimSpace(userAnswer)
@@ -76,5 +91,5 @@ func getAnswer(userAnswer string, quiz Quiz, score int) int {
 	if userAnswer == quiz.answer {
 		score++
 	}
-	return score
+	c <- score
 }
